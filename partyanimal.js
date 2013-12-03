536c32d85063e2a2b80e42cc44668bd030e67dc0
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2013-12-03 13:10:29 +0100

    Small refactoring to make injecting the pixel frames cleaner

--- partyanimal.js
+++ partyanimal.js
@@ -1,11 +1,27 @@
-window.partyanimal = window.partyanimal || {};
-window.partyanimal.party = function(canvas_el) {
-    // FIXME: ugly
-    var frames = window.partyanimal.frames;
+window.partyanimal = function() {
+    var canvas;
+    var frames;
+    // Misnomer; this is the delay between frames
+    var animSpeed = 150;
     var frameIdx;
-    var canvasWidth;
-    var canvasHeight;
+    var imgWidth;
+    var imgHeight;
     var ctx;
+
+    function centerInCanvas(imgWidth, imgHeight, canvasWidth, canvasHeight) {
+        // Returns (x, y) where (imgWidth, imgHeight) can be drawn
+        // and be centered in (canvasWidth, canvasHeight).
+
+        // FIXME: currently, this assumes img{Width,Height} <= canvas{Width,Height}
+        var widthOverflow = canvasWidth - imgWidth;
+        var heightOverflow = canvasHeight - imgHeight;
+
+        return (Math.floor(widthOverflow / 2), Math.floor(heightOverflow / 2));
+    }
+
+    function setFrames(frameList) {
+        frames = frameList;
+    }
 
     function drawPixel(imageData, x, y, color) {
         // imageData is flat, so we need to calculate where this goes.
@@ -20,23 +36,26 @@ window.partyanimal.party = function(canvas_el) {
         imageData.data[index + 3] = a;
     }
     
-    function animate() {
-        var canvas = document.getElementById(canvas_el);
-        canvasWidth = canvas.width;
-        canvasHeight = canvas.height;
+    function animate(canvas_el) {
+        canvas = document.getElementById(canvas_el);
         ctx = canvas.getContext("2d");
         frameIdx = 0;
-        return setInterval(draw, 150);
+        return setInterval(draw, animSpeed);
     }
 
     function clear() {
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
     function draw() {
+        if (! frames) {
+            return;
+        }
+
         clear();
         var imageData = ctx.createImageData(64, 50);
         var frame = frames[frameIdx];
+        var pos = centerInCanvas(canvas.width, canvas.height);
         for (i = 0; i < frame.length; i++) {
             x = i % 64;
             y = Math.floor(i / 64);
@@ -46,6 +65,10 @@ window.partyanimal.party = function(canvas_el) {
         frameIdx = (frameIdx + 1) % 4;
     }
     
-    animate();
-};
+
+    return {
+        party: animate,
+        setFrames: setFrames
+    }
+}();
 
